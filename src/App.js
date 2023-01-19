74be38aa8a198c5838a4043147d2b19beac2979a
azizkuchkarov <aziz_kuchkarov91@mail.ru>
2023-01-19 14:51:10 +0500

    This is a React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+const User = ({ firstname, lastName, link }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        Mening ismim - {firstname.name} and sharifim {lastName}
+      </h1>
+      <a href={link}>Youtube</a>;
     </div>
   );
-}
+};
+
+const App = () => {
+  return (
+    <div>
+      <User
+        firstname={{ name: "Aziz" }}
+        lastName="Kuchkarov"
+        link="youtube.com"
+      />
+      ;
+      <User
+        firstname={{ name: "Laziz" }}
+        lastName="Boqiev"
+        link="youtube.com"
+      />
+      ;
+    </div>
+  );
+};
 
 export default App;
